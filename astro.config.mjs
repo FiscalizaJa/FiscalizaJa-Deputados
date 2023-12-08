d5604f2b70cde42a79fb57716a31ba16ee9946cb
Victoreisdavid <65618679+Victoreisdavid@users.noreply.github.com>
2023-12-08 09:14:01 -0300

    Feat, Build: Instalação e configuração do @astrojs/node para ter server-side rendering.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://deputados.fiscalizaja.com",
   integrations: [react()],
-  output: "hybrid"
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
